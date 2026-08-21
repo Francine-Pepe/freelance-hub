172b20dd9a359353a0da6be8007e14ed8166c984
Francine-Pepe <franmelopepe@gmail.com>
2026-08-21 15:22:43 +0200

    refactor: pages refactored in order to use app.blade

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $client->name }}</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('title', $client->name)
+
+@section('content')
+
     <h1>{{ $client->name }}</h1>
 
     @if ($client->company)
@@ -22,5 +19,5 @@
     @endif
 
     <a href="/clients">Back to clients</a>
-</body>
-</html>
+
+@endsection
