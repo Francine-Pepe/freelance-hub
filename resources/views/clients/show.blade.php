c23df6fd8021bf69500ddd23d812867ad8364075
Francine-Pepe <franmelopepe@gmail.com>
2026-08-25 16:50:06 +0200

    blade pages

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -18,6 +18,36 @@
         <p>Phone: {{ $client->phone }}</p>
     @endif
 
+    <h2>Projects</h2>
+
+    @forelse ($client->projects as $project) {{-- this line is using this: public function projects(): HasMany --}}
+
+        <article>
+            <h3>
+                <a href="/projects/{{ $project->id }}">
+                    {{ $project->name }}
+                </a>
+            </h3>
+
+            @if ($project->budget)
+                <p>
+                    Budget:
+                    €{{ number_format($project->budget, 2, ',', '.') }}
+                </p>
+            @endif
+
+            <p>
+                Status: {{ $project->statusLabel() }}
+            </p>
+        </article>
+    @empty
+
+        <p>This client has no projects yet.</p>
+
+    @endforelse
+
+    <a href="/clients/{{ $client->id }}/edit">Edit Client</a>
+
     <a href="/clients">Back to clients</a>
 
 @endsection
