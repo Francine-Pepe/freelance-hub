29138b9a25c98188921b75321ff434dd62fdc946
Francine-Pepe <franmelopepe@gmail.com>
2026-09-07 15:44:56 +0200

    teste

--- public/index.php
+++ public/index.php
@@ -1,11 +1,19 @@
 <?php
 
 echo '<pre>';
+
 var_dump([
-    'SESSION_DRIVER' => getenv('SESSION_DRIVER'),
-    'APP_ENV' => getenv('APP_ENV'),
-    'APP_KEY_EXISTS' => !empty(getenv('APP_KEY')),
+    'SESSION_DRIVER_getenv' => getenv('SESSION_DRIVER'),
+    'SESSION_DRIVER_SERVER' => $_SERVER['SESSION_DRIVER'] ?? null,
+    'SESSION_DRIVER_ENV' => $_ENV['SESSION_DRIVER'] ?? null,
+
+    'APP_ENV_getenv' => getenv('APP_ENV'),
+    'APP_ENV_SERVER' => $_SERVER['APP_ENV'] ?? null,
+    'APP_ENV_ENV' => $_ENV['APP_ENV'] ?? null,
+
+    'APP_URL_getenv' => getenv('APP_URL'),
 ]);
+
 echo '</pre>';
 
 exit;
